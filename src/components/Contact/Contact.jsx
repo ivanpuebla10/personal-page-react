9a0b022730b436c2a9f83a5353dfc20c306337e9
ivanpuebla10 <ivanpuebla94@gmail.com>
2022-02-16 16:21:45 +0100

    personal page in react finished

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -19,42 +19,66 @@ export const Contact = () => {
   };
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log("sending data..." + data.name + " " + data.email);
+    console.log("sending data..." + data.firstname + " " + data.lastname + " " +data.email+ " " +data.birthdate);
     setTimeout(() => {
         navigate("/");
       }, 1000);
+      
+    let firstName = data.firstname;
+    let lastName = data.lastname;
+    let email = data.email;
+    let date = data.birthdate;
+
+    let dataBase = JSON.parse(localStorage.getItem('myform')) || [];
+
+    const information = {
+        firstName,
+        lastName,
+        email,
+        date   
+     }
+
+     dataBase.push(information)
+
+
+    localStorage.setItem('myform', JSON.stringify(dataBase));
   };
 
-
   return (
-    <form onSubmit={handleSubmit}>
+    <div className="form_container">
+    <form id = "myform" onSubmit={handleSubmit}>
       <input
         type="text"
         placeholder="firstname"
         onChange={handleInputChange}
         name="firstname"
+        id="firstname"
       />
       <input
         type="text"
         placeholder="lastname"
         onChange={handleInputChange}
         name="lastname"
+        id="lastname"
       />
       <input
         type="email"
         placeholder="email"
         onChange={handleInputChange}
         name="email"
+        id="email"
       />
       <input
         type="date"
         placeholder="birthdate"
         onChange={handleInputChange}
         name="birthdate"
+        id="birthdate"
       />
       
       <button type="submit">Send</button>
     </form>
+    </div>
   );
 };
 
